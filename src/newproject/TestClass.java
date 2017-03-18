e81f560e15bdbb7f39a4a4069635f4ca099963c6
GTRAsad <nayan_asad@yahoo.com>
2017-03-18 13:39:52 +0600

    Second Commit

--- src/newproject/TestClass.java
+++ src/newproject/TestClass.java
@@ -5,6 +5,9 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      System.out.print("Hello Class !!!");
+     
+     System.out.println("Hi Hi");
+     
 	}
 
 }
